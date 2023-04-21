1857f8540fb16697080aa7577334f3f4d56cae4e
Efya1235 <bkumah51@gmail.com>
2023-04-21 11:34:51 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  *main - Entry point
- *Description - 'printing last digit number'
+ *Description - 'last digit'
  *Return: always 0 (sucess)
  */
 int main(void)
@@ -28,3 +28,4 @@ int main(void)
 	}
 
 	return (0);
+}
