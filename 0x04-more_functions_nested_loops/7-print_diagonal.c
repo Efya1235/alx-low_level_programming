ab95c9863eb261a1f0d3071816d003b635c20670
Efya1235 <bkumah51@gmail.com>
2023-04-27 04:35:18 -0700

    parellel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print lines
+ * @n: input variable
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int y, z;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (y = 0; y < n; y++)
+	{
+		for (z = 0; z < y; z++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
