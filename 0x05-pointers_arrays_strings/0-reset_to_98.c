4379e457bcc9e4aae4347e22d9027ccad894441c
Efya1235 <bkumah51@gmail.com>
2023-05-02 01:33:43 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - batterry street updates the values it points
+ * writes a function that takes paremeter to an int
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
