55ae6b4bab66918add874b139721228f75ad884d
Efya1235 <bkumah51@gmail.com>
2023-05-24 07:11:06 -0700

    Task 0x0f

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to function of one of the 3 in main
+ * Return: 0 (success)
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int h;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (h = 0; h < size; h++)
+	{
+		if (cmp(array[h]))
+			return (h);
+	}
+	return (-1);
+}
+
