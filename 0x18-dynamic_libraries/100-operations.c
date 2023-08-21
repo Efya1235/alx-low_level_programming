1b68fe761055ea4ad143153a36f820fab6c7bf95
Efya1235 <bkumah51@gmail.com>
2023-08-21 09:41:08 -0700

    committed

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,11 @@
+// 100-operations.c
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to subtract one integer from another
+int subtract(int a, int b) {
+    return a - b;
+}
